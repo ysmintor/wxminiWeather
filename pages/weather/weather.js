f7edbccf161d5d33a205cb173385ad0d66b0e8cd
YorkYu <ysmintor@gmail.com>
2018-12-14 13:04:34 +0800

    增加获取位置信息并获取天气信息

---
+++ pages/weather/weather.js
@@ -0,0 +1,166 @@
+// pages/weather/weater.js
+var app = getApp()
+var day = ["今天", "明天", "后天"];
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    day: day,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    console.log('onLoad')
+    var that = this
+    that.getLocation();
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  },
+
+  //获取经纬度方法 
+  getLocation: function() {
+    var that = this
+    wx.getLocation({
+      type: 'wgs84',
+      success: function(res) {
+        var latitude = res.latitude
+        var longitude = res.longitude
+        console.log("lat:" + latitude + " lon:" + longitude);
+        that.getCity(latitude, longitude);
+      }
+    })
+  },
+  //获取城市信息
+  getCity: function(latitude, longitude) {
+    var that = this;
+    var url = "https://api.map.baidu.com/geocoder/v2/";
+    var params = {
+      ak: "MWlBFga2Mm2tcEpqZbbTKm6f67TgHs23",
+      output: "json",
+      location: latitude + "," + longitude
+    }
+    wx.request({
+      url: url,
+      data: params,
+      success: function(res) {
+        var city = res.data.result.addressComponent.city;
+        var district = res.data.result.addressComponent.district;
+        var street = res.data.result.addressComponent.street;
+        that.setData({
+          city: city,
+          district: district,
+          street: street,
+        })
+        var descCity = city.substring(0, city.length - 1);
+        that.getWeahter(descCity);
+      },
+      fail: function(res) {},
+      complete: function(res) {},
+    })
+  },
+
+  //获取天气信息
+  getWeahter: function(city) {
+    var that = this
+    var url = "https://free-api.heweather.com/s6/weather"
+    var url2 = "https://free-api.heweather.com/s6/air"
+    var parameters = {
+      location: city,
+      key: "eaed3a52d24f402e97b86d4f51a37873"
+    }
+    wx.request({
+      url: url,
+      data: parameters,
+      success: function(res) {
+        var tmp = res.data.HeWeather6[0].now.tmp;
+        var txt = res.data.HeWeather6[0].now.cond_txt;
+        var code = res.data.HeWeather6[0].now.cond_code;
+        var dir = res.data.HeWeather6[0].now.wind_dir;
+        var sc = res.data.HeWeather6[0].now.wind_sc;
+        var hum = res.data.HeWeather6[0].now.hum;
+        var fl = res.data.HeWeather6[0].now.fl;
+        var daily_forecast = res.data.HeWeather6[0].daily_forecast;
+        that.setData({
+          tmp: tmp,
+          txt: txt,
+          code: code,
+          dir: dir,
+          sc: sc,
+          hum: hum,
+          fl: fl,
+          daily_forecast: daily_forecast
+        })
+      },
+      fail: function(res) {},
+      complete: function(res) {},
+    })
+    wx.request({
+      url: url2,
+      data: parameters,
+      success: function(res) {
+        var qlty = res.data.HeWeather6[0].air_now_city.qlty;
+        var pm25 = res.data.HeWeather6[0].air_now_city.pm25;
+        // var air_forecast = res.data.HeWeather6[0].air_forecast;
+        that.setData({
+          qlty: qlty,
+
+          pm25: pm25
+          // air_forecast: air_forecast
+        })
+      },
+      fail: function(res) {},
+      complete: function(res) {},
+    })
+  }
+})
