19d7c96986d0f805aa87475618ecbedf109bbee7
YorkYu <ysmintor@gmail.com>
2018-12-15 15:36:57 +0800

    modify personal page description
     修改个人界面说明

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: '个人天气',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
